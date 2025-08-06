66459bf0fdb2b60a56f65228f9ce5abca1ce3a8d
Niladri Saha <bsensous@gmail.com>
2025-08-06 22:55:29 +0530

    Adding changes to the model

--- URLShorter/tinyurl/models.py
+++ URLShorter/tinyurl/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class URLStore(models.Model):
-    input_url = models.CharField(max_length=500)
+    input_url = models.CharField(max_length=500,unique=True)
     hash_url = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
